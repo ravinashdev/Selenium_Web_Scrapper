2d46754618a10ab6492a0bb2a10f254b62a04071
Ryan Ramdehol <ravinashwork@gmail.com>
2026-04-21 23:52:02 -0400

    had to use firefox since chrome kept crashing

---
+++ main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+driver.get("https://www.google.com")
+
+input("Press Enter to close...")
+driver.quit()
+
