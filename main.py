82a30e27e19dcb349ac82ccda61000173b342211
Ryan Ramdehol <ravinashwork@gmail.com>
2026-04-22 15:44:06 -0400

    obscured absolute path in .env file

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+# ---------------------------- IMPORTS ------------------------------- #
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
+# Allows you to read the .env file
+from dotenv import load_dotenv
+import os
+# ---------------------------- CONSTANTS ------------------------------- #
+load_dotenv()
 
+# ---------------------------- UI SETUP ------------------------------- #
 # Running Selenium with a profile so we can bypass some login issues
-profile_path = "/Users/ryanramdehol/Library/Application Support/Firefox/Profiles/vmjjns92.default-release"
+profile_path = os.environ.get("MOZILLA_PROFILE_PATH")
 options = Options()
 options.profile = profile_path
 driver = webdriver.Firefox(
